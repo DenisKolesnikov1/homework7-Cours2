e42557e473e7c2c0bacb244674008e4cb7966184
Денис Колесников <73371887@mail.ru>
2024-04-21 17:44:14 +0300

    hw7

--- src/main/java/pro/sky/arraylist/ArrayListApplication.java
+++ src/main/java/pro/sky/arraylist/ArrayListApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ArrayListApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ArrayListApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ArrayListApplication.class, args);
+	}
 
 }
---
+++ src/main/java/pro/sky/arraylist/Main.java
@@ -0,0 +1,11 @@
+package pro.sky.arraylist;
+
+import ch.qos.logback.classic.util.LogbackMDCAdapter;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
